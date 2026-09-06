b7abde5be73062dd4f2b7c3ee09f44b1caeefdd2
agent3030 <d.zozulya@outlook.com>
2026-09-06 21:11:20 +0300

    +seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Manager;
+use App\Models\Lead;
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -21,5 +24,45 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        $managerA = Manager::factory()->create([
+            'name' => 'Manager A',
+        ]);
+
+        $managerB = Manager::factory()->create([
+            'name' => 'Manager B',
+        ]);
+
+        $managerC = Manager::factory()->create([
+            'name' => 'Manager C',
+        ]);
+
+        Manager::factory()
+            ->inactive()
+            ->create([
+                'name' => 'Inactive Manager',
+            ]);
+
+        Lead::factory()
+            ->count(12)
+            ->assignedTo($managerA)
+            ->inProgress()
+            ->create();
+
+        Lead::factory()
+            ->count(4)
+            ->assignedTo($managerB)
+            ->inProgress()
+            ->create();
+
+        Lead::factory()
+            ->count(8)
+            ->assignedTo($managerC)
+            ->inProgress()
+            ->create();
+
+        Lead::factory()
+            ->count(9)
+            ->create();
     }
 }
